c5a217e1ee2189666c22cb1d832765ac0076b649
leen-neel <and24903@gmail.com>
2022-03-10 11:49:22 +0530

    structured the content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column q-gutter-md">
     <img src="/AppIcon.png" />
+    <div class="text-h6">
+      Greetings! The site has been moved to a new domain. Click
+      <a href="https://and249.codes/">here</a> if it doesn't redirect.
+    </div>
   </q-page>
 </template>
 
@@ -11,3 +15,18 @@ export default defineComponent({
   name: "PageIndex",
 });
 </script>
+
+<style lang="scss" scoped>
+img {
+  max-width: 100px;
+}
+
+a {
+  color: white;
+  text-decoration: underline;
+
+  &:hover {
+    text-decoration: none;
+  }
+}
+</style>
